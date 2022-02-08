ad16f2b5c03a0d770667a7bcf6fe8824d269e950
skd6150 <skd6150@gmail.com>
2022-02-08 16:43:20 +0900

    Updates

---
+++ component---src-pages-404-tsx-87f471a481b8d5b2ed25.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[218],{938:function(t,e,a){a.r(e);var n=a(7294),l=a(8466),s=a(9470);e.default=function(t){var e=t.data,a=t.location,o=e.site.siteMetadata.title;return n.createElement(l.Z,{location:a,title:o},n.createElement(s.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-87f471a481b8d5b2ed25.js.map
